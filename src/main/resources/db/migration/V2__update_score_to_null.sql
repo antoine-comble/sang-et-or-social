af9bb67e432d34ef756e57eaa8ae74861f3dc9b6
Antoine <antoine.comble@zenika.com>
2026-02-02 11:37:16 +0100

    BDD : - use Postgresql instead of hsql - init tables

---
+++ src/main/resources/db/migration/V2__update_score_to_null.sql
@@ -0,0 +1,19 @@
+-- FEVRIER
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (37, 'COUPE DE FRANCE', to_date('2026-02-04', 'YYYY-MM-DD'), false, 'ESTAC TROYES', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (21, 'LIGUE 1', to_date('2026-02-07', 'YYYY-MM-DD'), true, 'STADE RENNAIS FC', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (22, 'LIGUE 1', to_date('2026-02-14', 'YYYY-MM-DD'), false, 'PARIS FC', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (23, 'LIGUE 1', to_date('2026-02-21', 'YYYY-MM-DD'), true, 'AS MONACO FC', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (24, 'LIGUE 1', to_date('2026-02-27', 'YYYY-MM-DD'), false, 'RC STRASBOURG ALSACE', null, null);
+-- MARS
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (25, 'LIGUE 1', to_date('2026-03-08', 'YYYY-MM-DD'), true, 'FC METZ', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (26, 'LIGUE 1', to_date('2026-03-15', 'YYYY-MM-DD'), false, 'FC LORIENT', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (27, 'LIGUE 1', to_date('2026-03-22', 'YYYY-MM-DD'), true, 'ANGERS SCO', null, null);
+-- AVRIL
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (28, 'LIGUE 1', to_date('2026-04-05', 'YYYY-MM-DD'), false, 'LILLE OSC', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (29, 'LIGUE 1', to_date('2026-04-12', 'YYYY-MM-DD'), true, 'PARIS SG', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (30, 'LIGUE 1', to_date('2026-04-19', 'YYYY-MM-DD'), true, 'TOULOUSE FC', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (31, 'LIGUE 1', to_date('2026-04-26', 'YYYY-MM-DD'), true, 'STADE BRESTOIS 29', null, null);
+-- MAI
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (32, 'LIGUE 1', to_date('2026-05-03', 'YYYY-MM-DD'), false, 'OGC NICE', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (33, 'LIGUE 1', to_date('2026-05-09', 'YYYY-MM-DD'), true, 'FC NANTES', null, null);
+INSERT INTO match (id, competition, date, home, opponent, score_lens, score_opponent) VALUES (34, 'LIGUE 1', to_date('2026-05-16', 'YYYY-MM-DD'), false, 'OLYMPIQUE LYONNAIS', null, null);
